81e7f093267d49a0df1e67f45babdf42d448d0e1
Nick Wichert <nick.wichert@icloud.com>
2022-09-18 16:26:52 -0500

    location page updates

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -10,17 +10,6 @@
 
 <!-- This example requires Tailwind CSS v2.0+ -->
 <div class="bg-white">
-  <div class="mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-    <div class="text-center">
-      <p class="mt-4 text-sm">
-        <router-link to="/" class="font-medium text-blue-500"> Click here to return to home page </router-link>
-      </p>
-      <br>
-      <br>
-      <p class="mt-1 text-4xl font-bold tracking-tight text-indigo-600 sm:text-5xl lg:text-6xl">316 E. Silver Spring Dr. Suite 231</p><br>
-
-    </div>
-  </div>
 
 <div class="relative bg-white">
   <div class="lg:absolute lg:inset-0">
@@ -31,7 +20,7 @@
   <div class="relative px-4 pt-12 pb-16 sm:px-6 sm:pt-16 lg:mx-auto lg:grid lg:max-w-7xl lg:grid-cols-2 lg:px-8">
     <div class="lg:col-start-2 lg:pl-8">
       <div class="mx-auto max-w-prose text-base lg:ml-auto lg:mr-0 lg:max-w-lg">
-        <h2 class="font-semibold leading-6 text-indigo-600">Parking & Directions</h2>
+        <h2 class="font-semibold leading-6 text-indigo-600">316 E. Silver Spring Dr. Suite 231</h2>
         <h3 class="mt-2 text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">Whitefish Bay, WI</h3>
         <p class="mt-8 text-lg text-gray-500">From 10 Hour Parking Structure Or 2 Hour Surface Parking</p>
         <div class="prose prose-indigo mt-5 text-gray-500">
