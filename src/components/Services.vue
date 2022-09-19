0beacac84817ac73c5871c22098b6d5cba19ba35
Nick Wichert <nick.wichert@icloud.com>
2022-09-18 22:10:08 -0500

    services

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -44,7 +44,7 @@
             <svg class="absolute h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
             </svg>
-            <p class="ml-9 text-lg leading-6 font-medium text-gray-900">General Anxiety</p>
+            <p class="ml-9 text-lg leading-6 font-medium text-gray-900">Generalized Anxiety</p>
           </dt>
         </div>
 
